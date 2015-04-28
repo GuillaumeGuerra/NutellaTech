653b433197308793c38f2988c4f49ab4f529d70e
guillaumeguerra <gg.aime.la.nutella@gmail.com>
2015-04-28 17:52:56 +0200

    first step for Patch Manager tool

--- src/Patch Manager/app/view1/view1.js
+++ src/Patch Manager/app/view1/view1.js
@@ -1,14 +1,30 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp.view1', ['ngRoute', 'ui.grid'])
+//angular.module('myApp.view1', ['ngRoute'])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'view1/view1.html',
+            controller: 'View1Ctrl'
+        });
+    }])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+    .controller('View1Ctrl', ['$scope', function ($scope) {
+        $scope.jediMembers = [
+            {name: 'Yoda', rank: 'Greatest of all times'},
+            {name: 'ObiWan', rank: 'not great'},
+            {name: 'QuiGon', rank: 'poor'}
+        ];
+        $scope.selectedJediName = $scope.jediMembers[0].name;
+        $scope.selectedJediRank = $scope.jediMembers[0].rank;
 
-.controller('View1Ctrl', [function() {
+        $scope.selectJedi = function (jedi) {
+            $scope.selectedJediName = jedi.name;
+            $scope.selectedJediRank = jedi.rank;
+        };
 
-}]);
+        $scope.gridOptions = {
+            enableFiltering: true,
+            data:$scope.jediMembers
+        };
+    }]);
