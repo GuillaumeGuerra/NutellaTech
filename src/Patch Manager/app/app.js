1d97a8924016ac280aae270c9abfdc95caf31d0e
guillaumeguerra <gg.aime.la.nutella@gmail.com>
2015-04-30 15:42:21 +0200

    few additions

--- src/Patch Manager/app/app.js
+++ src/Patch Manager/app/app.js
@@ -2,12 +2,14 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'ui.grid',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+    'ngRoute',
+    'ui.grid',
+    'ui.grid.exporter',
+    'ui.grid.selection',
+    'myApp.view1',
+    'myApp.view2',
+    'myApp.version'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+    config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.otherwise({redirectTo: '/view1'});
+    }]);
--- src/Patch Manager/app/view1/view1.js
+++ src/Patch Manager/app/view1/view1.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute', 'ui.grid'])
+angular.module('myApp.view1', ['ngRoute', 'ui.grid', 'ui.grid.exporter', 'ui.grid.selection'])
 //angular.module('myApp.view1', ['ngRoute'])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/view1', {
@@ -11,9 +11,10 @@ angular.module('myApp.view1', ['ngRoute', 'ui.grid'])
 
     .controller('View1Ctrl', ['$scope', function ($scope) {
         $scope.jediMembers = [
-            {name: 'Yoda', rank: 'Greatest of all times'},
-            {name: 'ObiWan', rank: 'not great'},
-            {name: 'QuiGon', rank: 'poor'}
+            {name: 'Yoda', rank: 'Greatest of all times', level: 'Good'},
+            {name: 'ObiWan', rank: 'not great', level: 'Medium'},
+            {name: 'Mace', rank: 'not great', level: 'Medium'},
+            {name: 'QuiGon', rank: 'poor', level: 'GrandPa'}
         ];
         $scope.selectedJediName = $scope.jediMembers[0].name;
         $scope.selectedJediRank = $scope.jediMembers[0].rank;
@@ -23,8 +24,41 @@ angular.module('myApp.view1', ['ngRoute', 'ui.grid'])
             $scope.selectedJediRank = jedi.rank;
         };
 
+        $scope.removeJedi = function (data) {
+            alert('Killing ! ' + data.name);
+
+            for (var index in $scope.jediMembers) {
+                if($scope.jediMembers[index].name===data.name)
+                    $scope.jediMembers.splice(index,1);
+            }
+        };
+
         $scope.gridOptions = {
             enableFiltering: true,
-            data:$scope.jediMembers
+            data: $scope.jediMembers,
+            columnDefs: [
+                {field: 'name', cellClass: 'jediName'},
+                {field: 'rank'},
+                {
+                    field: 'level',
+                    cellClass: function (grid, row, col, rowRenderIndex, colRenderIndex) {
+                        if (grid.getCellValue(row, col) === 'Good') {
+                            return 'blue';
+                        }
+                        else if (grid.getCellValue(row, col) === 'Medium') {
+                            return 'orange';
+                        }
+                        else {
+                            return 'red';
+                        }
+                    }
+                },
+                {
+                    field: 'edit',
+                    cellTemplate: '<button type="button"  ng-click="grid.appScope.removeJedi(row.entity)" >Kill !</button> '
+                }
+            ],
+            enableGridMenu: true,
+            exporterMenuCsv: true
         };
     }]);
