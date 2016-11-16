daa487cc7fad91cca0d64795dd7677fdaeca2560
Guillaume Guerra <gg.aime.la.nutella@gmail.com>
2016-11-16 23:44:44 +0100

    new project for dotnet core logging framework testing

---
+++ src/DotNetCore.Logs/src/DotNetCore.Logs/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace DotNetCore.Logs
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var loggerFactory = new LoggerFactory()
+               .AddConsole()
+               .AddDebug();
+            var logger = loggerFactory.CreateLogger<Program>();
+            logger.LogInformation(
+              "This is a test of the emergency broadcast system.");
+        }
+    }
+}
